8b7cb51f5d8c0c0b029ad66ec8314d7b44237c96
msidiq2235 <muhsidiq2005@gmail.com>
2024-05-30 18:27:27 +0700

    12

--- CircularQueues/CircularQueues.cpp
+++ CircularQueues/CircularQueues.cpp
@@ -65,5 +65,19 @@ public:
 		int REAR_position = REAR;
 
 		//cek apakah antrian kosong
+		if (FRONT == -1) {
+			cout << "Queue is empty\n";
+			return;
+		}
+
+		cout << "\nElements inn the queue are...\n";
+
+		//jika FRONT_postion <= REAR_position, iterasi dari FRONT hingga REAR 
+		if (FRONT_position <= REAR_position) {
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << "   ";
+			}
+			cout << endl;
+		}
 	}
 };
