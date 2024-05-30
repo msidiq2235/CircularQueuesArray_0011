59feddce29352d64f0e8f9bc10afb7f14cd8ee5e
msidiq2235 <muhsidiq2005@gmail.com>
2024-05-30 17:41:18 +0700

    6

--- CircularQueues/CircularQueues.cpp
+++ CircularQueues/CircularQueues.cpp
@@ -17,10 +17,24 @@ public:
 		cin >> num;
 		cout << endl;
 
-		//cek apakah antriann penuh
+		//cek apakah antrian penuh
 		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
 			cout << "\nQueue overflow\n";
 			return;
 		}
+
+		//cek apakah antrian kosong
+		if (FRONT-- - 1) {
+			FRONT = 0;
+			REAR = 0;
+		}
+		else {
+			//jika REAR berada di posisi terakhir array, kembali ke awal array
+			if (REAR == max - 1)
+				REAR = 0;
+			else
+				REAR = REAR + 1;
+		}
+		queue_array[REAR] = num;
 	}
 };
