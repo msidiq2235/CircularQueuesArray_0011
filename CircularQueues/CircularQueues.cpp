8e7415db0931516a62c9fc216f59ae01f467b5b0
msidiq2235 <muhsidiq2005@gmail.com>
2024-05-30 18:32:19 +0700

    13

--- CircularQueues/CircularQueues.cpp
+++ CircularQueues/CircularQueues.cpp
@@ -79,5 +79,12 @@ public:
 			}
 			cout << endl;
 		}
+		else {
+			//jika FRONT_postion > REAR_position, iterasi dari FRONT hingga akhir array
+			while (FRONT_position <= max - 1) {
+				cout << queue_array[FRONT_position] << "   ";
+				FRONT_position++;
+			}
+		}
 	}
 };
